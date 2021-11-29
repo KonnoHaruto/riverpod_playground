d12c0f60df28b7b5b7170d1684c02c8925862e64
Haruto Konno <qingdoujinye@gmail.com>
2021-11-29 11:35:26 +0900

    feat(todo_controller.dart): StateNotifierProviderを追加
    StateNotifierProviderを用いて状態を操作するクラスを追加

---
+++ lib/controller/todo_controller/todo_controller.dart
@@ -0,0 +1,42 @@
+import 'package:riverpod/riverpod.dart';
+import 'package:riverpod_playground/model/todo.dart';
+
+
+// StateNotifierを継承したclass.
+// TodoのリストをStateに持ち、Todoを操作するクラス
+class TodoListNotifier extends StateNotifier<List<Todo>> {
+  TodoListNotifier() : super([]);
+
+  // 作成
+  void add(Todo todo) {
+    state = [...state, todo];
+  }
+
+  // 削除
+  void delete(String todoId) {
+    state = [
+      for (final todo in state)
+        if (todo.id != todoId) todo,
+    ];
+  }
+
+  // 完了状態を操作
+  void toggel(String todoId) {
+    state = [
+      for (final todo in state)
+      if (todo.id == todoId)
+        todo.copyWith(completed: !todo.completed)
+      else 
+        todo,
+    ];
+  }
+}
+
+
+// 作成したTodoListNotifierのProvider
+// 状態を格納し、実装したメソッドを通じて状態を公開・操作できるProvider.
+// StateProviderとの違いは状態を操作するメソッドをStateNotifierを継承したクラスに定義できること
+final todoListNotifierProvider =
+    StateNotifierProvider<TodoListNotifier, List<Todo>>((ref) {
+  return TodoListNotifier();
+});
