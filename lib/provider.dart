497faf1d8457a0c1f5a107cf0cb6b558b7e272be
Haruto Konno <qingdoujinye@gmail.com>
2021-11-29 11:52:36 +0900

    feat(provider.dart home_page.dart): resetCountProviderを追加

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,12 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
 // 外部からは変更できない値を公開できる基本的なProvider.
 // 内部からは変更可能。
 final appNameProvider = Provider((ref) => 'My Todo');
-
 
 // 外部から変更可能な値を公開できるProvider.
 // 現在選択されているアイテムやフィルタリングの状態など、
 // 単純な状態を保持するのに適している。
 final counterProvider = StateProvider((ref) => 0);
+
+
+// counterの値を0にするProvider.
+final resetCountProvider = StateProvider((ref) {
+  final count = ref.watch(counterProvider);
+  return  count * 0;
+});
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_playground/provider.dart';
 
+import '../provider.dart';
+
 class HomePage extends ConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -9,6 +11,7 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final appName = ref.watch(appNameProvider);
     final counter = ref.watch(counterProvider.notifier);
+    final resetCount = ref.watch(resetCountProvider);
     return Scaffold(
       appBar: AppBar(
         title: Text(appName),
@@ -22,10 +25,17 @@ class HomePage extends ConsumerWidget {
               style: const TextStyle(fontSize: 40),
             ),
             ElevatedButton(
-                onPressed: () {
-                  counter.update((state) => state + 1);
-                },
-                child: const Text('Increment')),
+              onPressed: () {
+                counter.update((state) => state + 1);
+              },
+              child: const Text('Increment'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                counter.update((state) => resetCount);
+              },
+              child: const Text('Reset'),
+            ),
           ],
         ),
       ),
