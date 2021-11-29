9fc12c4def34a57da8e2a2e5d6b2fd32b7173dbb
Haruto Konno <qingdoujinye@gmail.com>
2021-11-29 10:49:40 +0900

    feat(provider.dart): provider.dartを追加

---
+++ lib/provider.dart
@@ -0,0 +1,11 @@
+import 'package:riverpod/riverpod.dart';
+
+// 外部からは変更できない値を公開できる基本的なProvider.
+// 内部からは変更可能。
+final appNameProvider = Provider((ref) => 'My Todo');
+
+
+// 外部から変更可能な値を公開できるProvider.
+// 現在選択されているアイテムやフィルタリングの状態など、
+// 単純な状態を保持するのに適している。
+final counterProvider = StateProvider((ref) => 0);
