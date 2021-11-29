dd86aa368ec8d9789b70008f440cbda05073bf81
Haruto Konno <qingdoujinye@gmail.com>
2021-11-29 12:19:03 +0900

    style(provider.dart): appNameを変更

--- lib/provider.dart
+++ lib/provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // 外部からは変更できない値を公開できる基本的なProvider.
 // 内部からは変更可能。
-final appNameProvider = Provider((ref) => 'My Todo');
+final appNameProvider = Provider((ref) => 'カウンター');
 
 // 外部から変更可能な値を公開できるProvider.
 // 現在選択されているアイテムやフィルタリングの状態など、
