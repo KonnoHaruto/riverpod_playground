da94d3a60e11480d32e3129565e42a5c824d5cb9
Haruto Konno <qingdoujinye@gmail.com>
2021-11-29 10:50:12 +0900

    feat(main.dat): ProviderScopeを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(const App());
+import 'view/home_page.dart';
+
+void main() {
+  runApp(const ProviderScope(
+    child: App(),
+  ));
+}
 
 class App extends StatelessWidget {
-  const App({ Key? key }) : super(key: key);
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return const MaterialApp(
-      title: 'riverpod_playground',
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: const Text('App'),
-      ),
     );
   }
 }
